f3c7cbc557cca029af561ceef682d50bc51fe630
saeyeonn <sy01lim@naver.com>
2024-11-14 14:55:09 +0900

    Feat: Spring data jpa를 이용한 BaseEntity로 변경

--- src/main/java/gdsc/session/question/repository/QuestionRepository1.java
+++
@@ -1,7 +0,0 @@
-package gdsc.session.question.repository;
-
-import gdsc.session.question.domain.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository1 extends JpaRepository<Question, Long> {
-}
